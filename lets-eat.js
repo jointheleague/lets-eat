d1d71506c8179449f990581f082eef69259f3984
najclark <nicholas@californiaclarks.com>
2016-03-21 09:35:11 -0700

    Closes #29

--- lets-eat.js
+++ lets-eat.js
@@ -211,6 +211,19 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+  Houston.hide_collection(Meteor.users);
+  Houston.hide_collection(Houston._admins);
+
+  Houston.methods("markers", {
+    "Geocode": function (post) {
+      var address = post.street + ", " + post.city + ", " + post.state + " " + post.zipCode;
+      var geo = new GeoCoder();
+      var result = geo.geocode(address);
+      Markers.update({_id : post._id},{$set: {latitude : result[0].latitude, longitude : result[0].longitude}});
+      console.log("Lat: " + result[0].latitude + " Lon: " + result[0].longitude);
+      return post.name + " geocoded successfully.";
+    }
+  });
   updateDB = function(){
     Markers.find({ //for default 15
       latitude: undefined
