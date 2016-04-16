93f18d397ffe2aa1bab35514e471ae6edacf4fe6
Michaelmvv <bukkit.michaelmvv@gmail.com>
2016-04-16 15:46:17 -0700

    Now actually searches

--- client/search.js
+++ client/search.js
@@ -13,3 +13,25 @@ var markers = function() {
 Template.search.rendered = function() {
   Meteor.typeahead.inject();
 };
+Template.search.events({
+  "click #Find": function(e){
+    var map = GoogleMaps.get('map');
+    var location = document.getElementById('searchBar').value;
+    var location2 = Markers.findOne({name:location});
+    if (location2 !== undefined && location2 !== null) {
+      map.instance.panTo(new google.maps.LatLng(location2.latitude,location2.longitude));
+      map.instance.setZoom(14);
+    }else{
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode( { 'address': location}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          var adr = results[0].formatted_address;
+          map.instance.panTo(new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng()));
+          map.instance.setZoom(12);
+        } else {
+          alert("Geocode was not successful for the following reason: " + status);
+        }
+      });
+    }
+  }
+});
