d99af01203530219426f8406e88e4e7b348e9286
najclark <nicholas@californiaclarks.com>
2016-05-09 21:07:01 -0700

    Fixes #81

--- lets-eat.js
+++ lets-eat.js
@@ -116,14 +116,14 @@ if (Meteor.isClient) {
               });
               var currentImg;
               if (document.orgID.toUpperCase()==="SDFB") {
- +                currentImg='<img src="/SDFB.Color.Logo.PNG.png" style="width:100px;">';
-                }else if (document.orgID.toUpperCase()==="FASD") {
- +                currentImg='<img src="/FASD.Logo.CMYK.jpg" style="width:100px;">';
- +              }else if(document.orgID.toUpperCase()==="BOTH"){
- +                currentImg='<img src="/SDFB.Color.Logo.PNG.png" style="width:100px;"> <img src="/FASD.Logo.CMYK.jpg" style="position: absolute; right: 0; width:100px;">';
-                }else{
-                  currentImg="";
-                }
+                currentImg='<img src="/SDFB.Color.Logo.PNG.png" style="width:100px;">';
+              }else if (document.orgID.toUpperCase()==="FASD") {
+                currentImg='<img src="/FASD.Logo.CMYK.jpg" style="width:100px;">';
+              }else if(document.orgID.toUpperCase()==="BOTH"){
+                currentImg='<img src="/SDFB.Color.Logo.PNG.png" style="width:100px;"> <img src="/FASD.Logo.CMYK.jpg" style="position: absolute; right: 0; width:100px;">';
+              }else{
+                currentImg="";
+              }
 
               var urlInfo;
               if (typeof document.webURL !== 'undefined') {
