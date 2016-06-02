fc87970424955f350bb2f2f02489f6b93bb49414
Michaelmvv <bukkit.michaelmvv@gmail.com>
2016-06-02 11:59:56 -0700

    Add new line for thanks
    closes #99

--- lets-eat.js
+++ lets-eat.js
@@ -17,7 +17,7 @@ Markers.find().observe({
   }
 });
 Router.onAfterAction(function(){
-  var message = encodeURIComponent("To make sure we can help you quickly, please include the version of Lets Eat you are using, steps to replicate the issue, a description of what you were expecting and a screenshot if relevant. Thanks!");
+  var message = encodeURIComponent("To make sure we can help you quickly, please include the version of Lets Eat you are using, steps to replicate the issue, a description of what you were expecting and a screenshot if relevant.\nThanks!");
   var m2 = "https://github.com/jointheleague/lets-eat/issues/new?title=Your%20Lets%20Eat%20Issue&body="+message;
   $("a[id='houston-report-bug']").attr('href',m2);
 })
