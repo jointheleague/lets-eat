42ab5e3da7268b6f440b2dfaf67959d192c15e5b
Michaelmvv <mikey@Michaels-MacBook-Pro.local>
2016-12-13 17:41:26 -0800

    Backend isActive is now working

--- client/Foods.js
+++ client/Foods.js
@@ -2,7 +2,10 @@ Template.Foods.events({
   'click':function(e){
     if (e.target.type ==='checkbox') {
       console.log(e);
-      var a ='foods.'+ e.target.getAttribute('data-type');
+      var a =e.target.getAttribute('data-type');
+      if(a==='produce'|a==='canned'|a==='bread'|a==='cooked'|a==='snacks'|a==='government'|a==='boxed'|a==='dairy'|a==='produce2'|a==='storeDonations'){
+        a='foods.'+ a
+      }
       var b = e.target.getAttribute('data-id');
       var c = e.target.checked;
       var data = {
