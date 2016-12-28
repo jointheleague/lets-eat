84af2ba7f61d5c7309f3a136b2753fcad54214a6
Phil Cruz <phil@philcruz.com>
2016-12-28 09:05:49 -0800

    move routes.js

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,7 +1,10 @@
+import './routes.js';
+import '/imports/api/markers/data-table-config.js';
+
 import { $ } from 'meteor/jquery';
 import dataTablesBootstrap from 'datatables.net-bs';
 import 'datatables.net-bs/css/dataTables.bootstrap.css';
 dataTablesBootstrap(window, $);
 
 
-import '/imports/api/markers/data-table-config.js';
+
